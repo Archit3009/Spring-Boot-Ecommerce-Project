235788e73b99ade856500e8c832131fc9127f23e
Archit3009 <guptaarchit615@gmail.com>
2026-07-08 18:04:47 +0530

    configuring the project for JPA

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,12 +1,21 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
     private Long categoryId;
     private String categoryname;
 
     public Category(Long categoryId, String categoryname) {
         this.categoryId = categoryId;
         this.categoryname = categoryname;
+    }
+
+    public Category() {
+
     }
 
     public Long getCategoryId() {
